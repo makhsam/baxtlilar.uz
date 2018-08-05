9cbb57192100dec5000cb2269c70738c290d4df5
Sayidazim <sam1997co@mail.ru>
2018-08-05 12:44:05 +0500

    fixed navbar and slideshow added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,43 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function nextSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("slideshow-cover");
+	//var slides = $(".slideshow-cover");
+	if (n > slides.length) {slideIndex = 1}    
+	if (n < 1) {slideIndex = slides.length}
+	//slides.css("display", "none");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";  
+	}
+	
+	slides[slideIndex-1].style.display = "block";  
+	//slides.eq(slideIndex-1).css("display", "block");
+}
+
+
+/* ---- JQuery ---- */
 ;(function () {
 	
 	'use strict';
+
+	/** Navbar fixed top */
+	var navbarFixed = function() {
+
+		$(window).scroll(function () {
+			if ($(window).scrollTop() >= 200) {
+				$('.fh5co-nav-fixed').css('top','0');
+			} else {
+				$('.fh5co-nav-fixed').css('top','-100px');
+			}
+		});
+	}
+
 
 	var mobileMenuOutsideClick = function() {
 
@@ -140,6 +177,31 @@
 
 	};
 
+	// FOR FIXED NAVBAR (slide down on.scroll)
+	var dropdown_fixed = function() {
+
+		$('.has-dropdown-fixed').mouseenter(function(){
+			console.log("Dropdown Mouse Enter");
+
+			var $this = $(this);
+			$this
+				.find('.dropdown-fixed')
+				.css('display', 'block')
+				.addClass('animated-fast fadeInUpMenu');
+
+		}).mouseleave(function(){
+			console.log("Dropdown Mouse leave");
+
+			var $this = $(this);
+
+			$this
+				.find('.dropdown-fixed')
+				.css('display', 'none')
+				.removeClass('animated-fast fadeInUpMenu');
+		});
+
+	};
+
 
 	var testimonialCarousel = function(){
 		var owl = $('.owl-carousel-fullwidth');
@@ -215,12 +277,14 @@
 
 	
 	$(function(){
+		navbarFixed();
 		mobileMenuOutsideClick();
 		parallax();
 		offcanvasMenu();
 		burgerMenu();
 		contentWayPoint();
 		dropdown();
+		dropdown_fixed();
 		testimonialCarousel();
 		goToTop();
 		loaderPage();
